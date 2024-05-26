3c710167b801041979b1ce7cac9d1b7cdeae136d
Faisal Aslam <faisalaslam218@gmail.com>
2024-05-27 03:47:15 +0500

    Classes and Objects

---
+++ 3. Classes_and_Objects.dart
@@ -0,0 +1,22 @@
+// A class is a blueprint for creating objects.
+class MyCar {
+  // attributes
+  String? Carname;
+  int? carModel;
+
+  // Functions
+  void display() {
+    print("The name of car is $Carname");
+    print("The Model of car is $carModel");
+  }
+}
+
+void main() {
+  // object is anything e.g person , car , watch , pen is a object.
+  // Now lets create object of car class.
+  MyCar myCar = MyCar();
+  myCar.Carname = "Civic";
+  myCar.carModel = 2020;
+
+  myCar.display();
+}
